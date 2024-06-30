e456efca63689b853f30b011803b20848b6ec73d
Volha_Bakharava <olavlad777@gmail.com>
2024-06-30 20:14:59 +0300

    AWS-RS-HW-5 Adopted FE part

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://dd84tfs562.execute-api.eu-west-1.amazonaws.com/prod/",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://dd84tfs562.execute-api.eu-west-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
